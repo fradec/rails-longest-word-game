989ed85e458526e94651a9b2eb1458bb308f410e
Arnaud de Castelbajac <adecastelbajac@gmx.com>
2019-08-12 17:09:03 +0200

    more css and view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,18 +7,18 @@ class GamesController < ApplicationController
   end
 
   def score
-    input = params[:word_input]
+    input = params[:word_input].upcase
     array = params[:letters].split
     url = "https://wagon-dictionary.herokuapp.com/#{input}"
     serialized_wagon_dic = open(url).read
     word = JSON.parse(serialized_wagon_dic)
-    falsy = input.upcase.split('').map { |char| array.include?(char) ? array.delete_at(array.index(char)) : 'false' }
+    falsy = input.split('').map { |char| array.include?(char) ? array.delete_at(array.index(char)) : 'false' }
     if word['found'] == false
-      @response = "Sorry, but #{input.upcase} does not seems to be a valid English word"
+      @response = "Sorry, <strong>#{input}</strong> doesn't seem to be a valid English word".html_safe
     elsif falsy.include? 'false'
-      @response = "Sorry, but #{input.upcase} can't be built out of #{array}"
+      @response = "Sorry, #{input} can't be built out of #{array}"
     else
-      @response = "Congratulations! #{input.upcase} is a valid English word"
+      @response = "Congratulations! #{input} is a valid English word"
     end
   end
 end
