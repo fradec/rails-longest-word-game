d88ac0b412826783c9d1e8131831543e9cb0a8c5
Arnaud de Castelbajac <adecastelbajac@gmx.com>
2019-08-12 16:23:16 +0200

    score building

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,24 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
+    @letters = Array.new(10) { [*'A'..'Z'].sample }
   end
 
   def score
+    input = params[:word_input]
+    array = params[:letters].split
+    url = "https://wagon-dictionary.herokuapp.com/#{input}"
+    serialized_wagon_dic = open(url).read
+    word = JSON.parse(serialized_wagon_dic)
+    falsy = input.upcase.split('').map { |char| array.include?(char) ? array.delete_at(array.index(char)) : 'false' }
+    if word['found'] == false
+      @response = "Sorry, but #{input.upcase} does not seems to be a valid English word"
+    elsif falsy.include? 'false'
+      @response = "Sorry, but #{input.upcase} can't be built out of #{array}"
+    else
+      @response = "Congratulations! #{input.upcase} is a valid English word"
+    end
   end
 end
